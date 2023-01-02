357adf3b761603ac169f1965181d3b602a4eb111
Aman Khadka <amulifts@gmail.com>
2023-01-02 16:56:09 +0545

    bot

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -2,8 +2,9 @@ import random
 from datetime import timedelta
 from datetime import datetime
 from git import Repo
+from gitdb.util import Actor as actor
 
-repo_path = ''
+repo_path = '/path/to/repo'
 repo = Repo(repo_path)
 
 # Generate a random number of days between 0 and 25
@@ -20,8 +21,8 @@ with open(f'{repo_path}/test.txt', 'w') as f:
 repo.index.add(['test.txt'])
 
 # Create the commit
-author = ('Aman Khadka', 'amulifts@gmail.com')
-committer = ('Aman Khadka', 'amulifts@gmail.com')
+author = actor.Actor('Aman Khadka', 'amulifts@gmail.com')
+committer = actor.Actor('Aman Khadka', 'amulifts@gmail.com')
 repo.index.commit('Commit message', author=author, committer=committer, author_date=commit_date, commit_date=commit_date)
 
 # Push the commit to the repository
